78f9a0d31c2198d2005e6a66782b9b6ae6273968
quangcommerce <quangnc4874@yahoo.com>
2015-10-11 23:32:51 +0700

    Added Quang.Cate.*
    Quan ly danh muc

--- Presentation/Quang.Auth.Api/Controllers/HomeController.cs
+++ Presentation/Quang.Auth.Api/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StackExchange.Exceptional;
+using StackExchange.Exceptional.MySQL;
 
 namespace Quang.Auth.Api.Controllers
 {
@@ -12,8 +14,17 @@ namespace Quang.Auth.Api.Controllers
         // GET: Home
         public ActionResult Index()
         {
+          
+           
             return View();
         }
+        public ActionResult Exceptions()
+        {
+            var context = System.Web.HttpContext.Current;
+            var page = new HandlerFactory().GetHandler(context, Request.RequestType, Request.Url.ToString(), Request.PathInfo);
+            page.ProcessRequest(context);
 
+            return null;
+        }
     }
 }
