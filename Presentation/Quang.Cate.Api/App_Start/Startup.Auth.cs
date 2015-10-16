5d41deba8b6c606c70f9ce83cf3fca149fea4b96
quangnc4874 <quangnc4874@yahoo.com>
2015-10-16 15:32:16 +0700

    Add Quang.Cate.Api
    ff

---
+++ Presentation/Quang.Cate.Api/App_Start/Startup.Auth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Quang.Cate.Api.Providers;
+using Quang.Cate.Api.Models;
+using Quang.Common.Auth;
+namespace Quang.Cate.Api
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+            {
+                Provider = new ApplicationOAuthProvider()
+            });
+        }
+    }
+}
---
+++ Presentation/Quang.Cate.Api/Providers/ApplicationOAuthProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Quang.Cate.Api.Models;
+using Quang.Common.Auth;
+using System.Net.Http.Headers;
+
+namespace Quang.Cate.Api.Providers
+{
+    public class ApplicationOAuthProvider : OAuthBearerAuthenticationProvider
+    {
+        public override Task RequestToken(OAuthRequestTokenContext context)
+        {
+            var headers = context.Request.Headers;
+            var authorization = headers.Get(ClientApiProvider.ClientHeaderAuthName);
+            if (!string.IsNullOrEmpty(authorization))
+            {
+                var authentication = AuthenticationHeaderValue.Parse(authorization);
+                if (authentication != null && authentication.Scheme == ClientApiProvider.ClientHeaderAuthScheme)
+                {
+                    string accessToken = ClientApiProvider.GetAccessToken(authentication, context.OwinContext.Request).Result;
+                    if (!string.IsNullOrEmpty(accessToken))
+                    {
+                        var authorizationHeader = new AuthenticationHeaderValue(ClientApiProvider.OAuthHeaderScheme, accessToken).ToString();
+                        context.Request.Headers.Set(ClientApiProvider.OAuthHeaderName, authorizationHeader);
+                        context.Token = accessToken;
+                    }
+                }
+            }
+
+            return base.RequestToken(context);
+        }
+    }
+}
