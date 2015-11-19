edc5410623c0139e336d0d37e48a8fdd093c3f31
quangnc4874 <quangnc4874@yahoo.com>
2015-11-19 16:52:42 +0700

    Add Class DistrictDal

---
+++ Libraries/Quang.Cate.Entities/Ward.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quang.Cate.Entities
+{
+    public class Ward
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ShortName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public long DistrictId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
